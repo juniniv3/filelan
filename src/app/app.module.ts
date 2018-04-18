924ae123f8bfca2c147853732fa7ebb586327be1
juniniv3 <juniniprogramador@outlook.com>
2018-04-17 22:08:21 -0500

    loginListo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PrincipalComponent } from './principal/principal.component';
 
+
+const routes: Routes =[
+  {path: '', component:InicioComponent},
+  {path: 'inicio', component:InicioComponent},
+  {path:'login', component:LoginComponent},
+  {path:'principal',component:PrincipalComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
